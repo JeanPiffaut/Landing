96797c2946b98fbb3bec69df9db456d96fac6e02
jeanpiffaut <piffaut.jean@gmail.com>
2022-03-27 21:48:30 -0300

    change content to not found page

--- controller/Not_Found.php
+++ controller/Not_Found.php
@@ -2,5 +2,22 @@
 
 class Not_Found extends Master_Page
 {
+    public string $title = "404 - Page not found";
 
+    public function Content()
+    {
+        ?>
+        La pagina buscada no existe
+        <?php
+    }
+
+    public function Header()
+    {
+
+    }
+
+    public function Footer()
+    {
+
+    }
 }
